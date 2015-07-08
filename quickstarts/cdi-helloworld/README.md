fd349f9c5e8e07b41d81022397cc1e9c5776b226
Martin Večeřa <marvenec@gmail.com>
2015-07-08 16:16:33 +0200

    cdi helloworld

---
+++ quickstarts/cdi-helloworld/README.md
@@ -0,0 +1,2 @@
+mvn clean package
+java -jar target/cdi-helloworld-*.jar
---
+++ quickstarts/cdi-helloworld/src/main/java/org/silverware/demos/quickstarts/cdi/CdiHelloWorldMicroservice.java
@@ -0,0 +1,57 @@
+/*
+ * -----------------------------------------------------------------------\
+ * SilverWare
+ *  
+ * Copyright (C) 2010 - 2013 the original author or authors.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -----------------------------------------------------------------------/
+ */
+package org.silverware.demos.quickstarts.cdi;
+
+import org.silverware.microservices.annotations.Microservice;
+import org.silverware.microservices.annotations.MicroserviceReference;
+import org.silverware.microservices.providers.cdi.MicroservicesStartedEvent;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @author Martin Večeřa <marvenec@gmail.com>
+ */
+@Microservice
+public class CdiHelloWorldMicroservice {
+
+   private static final Logger log = LogManager.getLogger(CdiHelloWorldMicroservice.class);
+
+   public CdiHelloWorldMicroservice() {
+      log.info("CdiHelloWorldMicroservice constructor");
+   }
+
+   @PostConstruct
+   public void onInit() {
+      log.info("CdiHelloWorldMicroservice PostConstruct " + this.getClass().getName());
+   }
+
+   public void hello() {
+      log.info("CdiHelloWorldMicroservice Hello");
+   }
+
+   public void eventObserver(@Observes MicroservicesStartedEvent event) {
+      log.info("CdiHelloWorldMicroservice MicroservicesStartedEvent");
+   }
+}
